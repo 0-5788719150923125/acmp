ed026185ac82170be746c6aae86e1b93a535fc8f
Luciferian Ink <LuciferianInk@protonmail.com>
2024-09-19 21:52:34 -0500

    pause

--- coqui.sh
+++ coqui.sh
@@ -2,8 +2,10 @@
 
 export CUDA_VISIBLE_DEVICES=""
 
+TEXT="[clears throat] I never really understood why the quick brown fox jumped over a lazy dog? I mean, what was the point? [laughs] Was the fox trying to get eaten?"
+
 tts --model_name "tts_models/multilingual/multi-dataset/bark" \
-    --text "Hello." \
+    --text "$TEXT" \
     --out_path /home/crow/repos/acmp/data/tts-output/hello.wav
     # --use_cuda false
     # --device cuda:0
